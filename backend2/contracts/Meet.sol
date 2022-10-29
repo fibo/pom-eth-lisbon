64c969507ebb5da84512122547e6d4ab54e6ea7b
Airton <ricardoairtonsilva@hotmail.com>
2022-10-29 18:12:47 +0100

    moved to hardhat

---
+++ backend2/contracts/Meet.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Meet {
+  string metadata;//check visibility chosen by the users
+  
+  //check visibility to make the connections available to mutual connections 
+  address owner1;
+  address owner2;
+
+  constructor(address _owner1, address  _owner2, string memory _metadata){
+    owner1 = _owner1;
+    owner2 = _owner2;
+    metadata = _metadata;
+  }
+
+  modifier isOwner(address _owner){
+    require(_owner == owner1 || _owner == owner2);
+    _;
+  }
+
+  //check if the input message sender is correct for this modifier verification
+  function getMetadata() external view isOwner(msg.sender) returns (string memory){
+    return metadata;
+  }
+}
---
+++ backend2/contracts/TwoUserMultiSig.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
+import "@matterlabs/zksync-contracts/l2/system-contracts/TransactionHelper.sol";
+import "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IAccount.sol";
+import "@matterlabs/zksync-contracts/l2/system-contracts/SystemContractsCaller.sol";
+
+contract TwoUserMultisig is IAccount, IERC1271 {
+    
+    using TransactionHelper for Transaction;
+
+    address public owner1;
+    address public owner2;
+
+    bytes4 constant EIP1271_SUCCESS_RETURN_VALUE = 0x1626ba7e;
+
+    modifier onlyBootloader() {
+        require(
+            msg.sender == BOOTLOADER_FORMAL_ADDRESS,
+            "Only bootloader can call this method"
+        );
+        // Continue execution if called from the bootloader.
+        _;
+    }
+
+    constructor(address _owner1, address _owner2) {
+        owner1 = _owner1;
+        owner2 = _owner2;
+    }
+
+    function validateTransaction(
+        bytes32,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        _validateTransaction(_suggestedSignedHash, _transaction);
+    }
+
+    function _validateTransaction(
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) internal {
+        // reserved[0] by convention is equal to the nonce passed in the transaction
+        SystemContractsCaller.systemCall(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(
+                INonceHolder.incrementMinNonceIfEquals,
+                (_transaction.reserved[0])
+            )
+        );
+
+        bytes32 txHash;
+        if (_suggestedSignedHash == bytes32(0)) {
+            txHash = _transaction.encodeHash();
+        } else {
+            txHash = _suggestedSignedHash;
+        }
+
+        require(
+            isValidSignature(txHash, _transaction.signature) == 
+            EIP1271_SUCCESS_RETURN_VALUE
+        );
+    }
+
+    function executeTransaction(
+        bytes32,
+        bytes32,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        _executeTransaction(_transaction);
+    }
+
+    function _executeTransaction(Transaction calldata _transaction) internal {
+        address to = address(uint160(_transaction.to));
+        uint256 value = _transaction.reserved[1]; // TODO Value might always be 0
+        bytes memory data = _transaction.data;
+
+        //TODO maybe we won't need to allow call contract deployer with calldata
+        if(to == address(DEPLOYER_SYSTEM_CONTRACT)) {
+            SystemContractsCaller.systemCall(
+                uint32(gasleft()),
+                to, 
+                uint128(_transaction.reserved[1]),
+                _transaction.data
+            )
+        } else {
+            bool success;
+            assembly {
+                success := call(
+                    gas(), to, value, add(data, 0x20), mload(data), 0, 0
+                )
+            }
+            require(success);
+        }
+    }
+
+    function executeTransactionFromOutside(Transaction calldata _transaction)
+        external
+        payable
+    {
+        _validateTransaction(bytes32(0), _transaction);
+
+        _executeTransaction(_transaction);
+    }
+
+    function isValidSignature(bytes32 _hash, bytes calldata _signature)
+        public
+        view
+        override
+        returns (bytes4)
+    {
+        // The signature is the concatenation of the ECDSA signatures of the owners
+        // Each ECDSA signature is 65 bytes long. That means that the combined signature is 130 bytes long.
+        require(_signature.length == 130, "Signature length is incorrect");
+
+        address recoveredAddr1 = ECDSA.recover(_hash, _signature[0:65]);
+        address recoveredAddr2 = ECDSA.recover(_hash, _signature[65:130]);
+        require(recoveredAddr1 == owner1);
+        require(recoveredAddr2 == owner2);
+
+        return EIP1271_SUCCESS_RETURN_VALUE;
+
+    }
+
+    function payForTransaction(
+        bytes32,
+        bytes32,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        bool success = _transaction.payToTheBootloader();
+        require(success, "Failed to pay the fee to the operator");
+    }
+
+    function prePaymaster(
+        bytes32,
+        bytes32,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        _transaction.processPaymasterInput();
+    }
+
+    receive() external payable {
+        // If the bootloader called the `receive` function, it likely means
+        // that something went wrong and the transaction should be aborted. The bootloader should
+        // only interact through the `validateTransaction`/`executeTransaction` methods.
+        assert(msg.sender != BOOTLOADER_FORMAL_ADDRESS);
+    }
+}
